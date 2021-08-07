f50313f01f89407667a2d4eaee977b257491638a
busimaru <ntdexam@icloud.com>
2021-08-07 13:49:09 +0900

    新規登録機能導入

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :name_first, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :name_family, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :name_kana_family, presence: true,format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :name_kana_first, presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true 
+end
