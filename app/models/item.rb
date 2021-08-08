770fbee9a8b56493f526e5d2797f909487c59df5
busimaru <ntdexam@icloud.com>
2021-08-08 20:03:35 +0900

    モデルの単体テストコード実装/バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,20 +5,21 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
   belongs_to :delivery_fee
   belongs_to :prefecture
-
   belongs_to :user
 
   has_one_attached :image
+  with_options presence:true do
+    validates :image
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :name,  length: { maximum: 40 }
+    validates :text,  length: { maximum: 1000 }
 
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :name, length: { maximum: 40 }
-  validates :text, length: { maximum: 1000 }
-
-  with_options numericality: { other_than: 1, message: "can't be blank" }  do
-    validates :category_id
-    validates :condition_id
-    validates :delivery_day_id
-    validates :delivery_fee_id
-    validates :prefecture_id
+    with_options numericality: { other_than: 1, message: "can't be blank" }  do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_day_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+    end
   end
 end
